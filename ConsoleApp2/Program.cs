dc9a66e994448ea94ca0f9b63557eeaace74c7da
Avazbek Olimov <avazbekolimov722@gmail.com>
2024-04-17 11:01:18 +0300

    Added parsing

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-class GrammarRule
+public class GrammarRule
 {
     public char NonTerminal { get; set; }
     public List<string> Productions { get; set; }
@@ -13,7 +14,7 @@ class GrammarRule
     }
 }
 
-class MainClass
+public class MainClass
 {
     static List<GrammarRule> RemoveRightRecursion(List<GrammarRule> grammarRules)
     {
@@ -51,23 +52,73 @@ class MainClass
         return newGrammarRules;
     }
 
+    public static List<GrammarRule> SplitLinesForGrammarRule(List<string> lines)
+    {
+        bool IsCorrectLine(string line)
+        {
+            return line.Length > 2 && (char.IsLetter(line[0]) && char.IsUpper(line[0]) || char.IsDigit(line[0]))
+                                   && line[1] == '-' && line[2] == '>';
+        }
+
+        List<GrammarRule> grammarRules = new List<GrammarRule>();
+
+        for (int i = 0; i < lines.Count; i++)
+        {
+            if (IsCorrectLine(lines[i]))
+            {
+                if (grammarRules.Any(rules => rules.NonTerminal == lines[i][0]) == false) // if character not existing
+                    grammarRules.Add(new GrammarRule(lines[i][0],
+                        lines[i].Remove(0, 3).Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToList()));
+                else
+                {
+                    int index = grammarRules.FindIndex(rules => rules.NonTerminal == lines[i][0]);
+                    grammarRules[index].Productions.AddRange(lines[i].Remove(0, 3)
+                        .Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToList());
+                }
+            }
+        }
+
+        return grammarRules;
+    }
+
     public static void Main(string[] args)
     {
         // Пример входной КС-грамматики с правой рекурсией
-        var grammarRules = new List<GrammarRule>
-        {
-            new GrammarRule('S', new List<string> { "SaA", "SbA","AA" }),
-            new GrammarRule('A', new List<string> { "AdB", "AcB","BB" }),
-            new GrammarRule('B', new List<string> { "SSc", "g", "f" })
-        };
+        // var grammarRules = new List<GrammarRule>
+        // {
+        //     new GrammarRule('S', new List<string> { "SaA", "SbA","AA" }),
+        //     new GrammarRule('A', new List<string> { "AdB", "AcB","BB" }),
+        //     new GrammarRule('B', new List<string> { "SSc", "g", "f" })
+        // };
+        //
+        // var newGrammarRules = RemoveRightRecursion(grammarRules);
+        //
+        // foreach (var rule in newGrammarRules)
+        // {
+        //     Console.WriteLine(rule.NonTerminal + " -> " + string.Join(" | ", rule.Productions));
+        //     
+        // }
+        // Console.ReadKey();
+        var testList = new List<string>();
+        testList.Add("S->SaA|SbA|AA|AD");
+        testList.Add("A->AdB|AcB|BB");
+        testList.Add("B->SSc|g|f");
+        testList.Add("S->SaA|SbA|AA|AD");
+        testList.Add("1->AdB|AcB|BB");
 
-        var newGrammarRules = RemoveRightRecursion(grammarRules);
+        // Incorrect data
+        testList.Add("AS->SaD");
+        testList.Add("1->");
+        testList.Add("a-z->");
+        testList.Add("");
+        testList.Add("->");
+        testList.Add("G->||,|");
 
-        foreach (var rule in newGrammarRules)
+        var grammarRules = SplitLinesForGrammarRule(testList);
+
+        foreach (var rule in grammarRules)
         {
-            Console.WriteLine(rule.NonTerminal + " -> " + string.Join(" | ", rule.Productions));
-            
+            Console.WriteLine(string.Join("|", rule.Productions));
         }
-        Console.ReadKey();
     }
 }
--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,28 +1,95 @@
 ﻿using System;
+using Microsoft.Win32;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp3
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // Список для хранения строк из файла
+        private List<string> _lines;
+
+        // Список для хранения объектов типа GrammarRule
+        private List<GrammarRule> _grammarRules;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Обработчик события нажатия кнопки загрузки файла
+        private void FileLoad_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка, что текстовое поле пустое
+            if (string.IsNullOrEmpty(InputTextBox.Text) == false)
+            {
+                MessageBox.Show("InputTextBox is not empty!");
+                return;
+            }
+
+            // Создание диалогового окна для выбора файла
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                InitialDirectory = @"C:\"
+            };
+
+            // Показ диалогового окна и обработка выбранного файла
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _lines = new List<string>();
+
+                try
+                {
+                    // Считывание строк из выбранного файла
+                    using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            _lines.Add(line);
+                        }
+                    }
+
+                    // Обработка строк для создания объектов GrammarRule
+                    _grammarRules = MainClass.SplitLinesForGrammarRule(_lines); 
+                    
+                    // Добавление строк в InputTextBox
+                    InputTextBox.Text = string.Join("\n", _lines);
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show($"Ошибка при чтении файла: {ex.Message}");
+                }
+            }
+        }
+
+        // Обработчик события нажатия кнопки Start
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            _lines = new List<string>();
+
+            try
+            {
+                // Считывание строк из InputTextBox и добавление их в список
+                _lines = InputTextBox.Text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                // Обработка строк для создания объектов GrammarRule
+                _grammarRules = MainClass.SplitLinesForGrammarRule(_lines); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Пустой метод для обработки данных после нажатия кнопки Start
+        private void StartProcessing()
+        {
+            // Ваша логика обработки здесь
         }
     }
 }
